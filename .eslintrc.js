ed88ae5c8e3f950a7723c1a2457cb88a55e469ac
Toan <nguyentoan04.0003@email.com>
2025-01-15 17:04:45 +0700

    1:51:15

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,11 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint', 'prettier'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'prettier',
   ],
   root: true,
   env: {
@@ -21,5 +22,14 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false, // Không sử dụng dấu chấm phẩy
+      },
+    ],
+    semi: ['error', 'never'], // Tắt bắt buộc dấu chấm phẩy
+    '@typescript-eslint/no-unused-vars': 'off', // Tắt quy tắc no-unused-vars
+    'no-unused-vars': 'off', // Tắt cảnh báo no-unused-vars từ ESLint mặc định
   },
-};
+}
