24ccc9d6bbb92fa79d9f742b5a4309e22d7c5077
Toan <nguyentoan04.0003@email.com>
2025-01-20 22:51:56 +0700

    register send email verify code

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,10 +4,13 @@ import { LocalAuthGuard } from './passport/local-auth.guard'
 import { JwtAuthGuard } from './passport/jwt-auth.guard'
 import { Public } from '@/decorator/customize'
 import { CreateAuthDto } from './dto/create-auth.dto'
+import { MailerService } from '@nestjs-modules/mailer'
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+  ) {}
 
   // su dung chien luoc local
   @UseGuards(LocalAuthGuard)
@@ -27,6 +30,27 @@ export class AuthController {
   async register(@Body() registertDto: CreateAuthDto) {
     return this.authService.handlerRegister(registertDto)
   }
+
+  // @Post('mail')
+  // @Public()
+  // async sendMail() {
+  //   try {
+  //     const result = await this.mailerService.sendMail({
+  //       to: 'nguyentoan04.0003@gmail.com', // list of receivers
+  //       from: 'noreply@nestjs.com', // sender address
+  //       subject: 'Testing Nest MailerModule ✔', // Subject line
+  //       text: 'Welcome', // plaintext body
+  //       template: 'template_verify',
+  //       context: {
+  //         name: 'Toan',
+  //         activationCode: '12345678',
+  //       },
+  //     })
+  //     return { success: true, message: 'Email sent successfully', result }
+  //   } catch (error) {
+  //     return { success: false, message: 'Failed to send email', error }
+  //   }
+  // }
 
   @Get('profile')
   getProfile(@Request() req) {
